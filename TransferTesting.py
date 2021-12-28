7cebf24021b2b22d9c16e0abf301a18703ff8d73
The Network & Software Security Research (NSSR) lab <86532171+nssrlab@users.noreply.github.com>
2021-12-27 21:33:51 -0500

    Update TransferTesting.py

--- TransferTesting.py
+++ TransferTesting.py
@@ -4,7 +4,7 @@ import torchvision.models as models
 import torchvision.datasets as datasets
 from PIL import Image
 from deepfool import deepfool
-from foolx import deepfool_hybrid2
+from foolx import foolx
 from models.AlexNet import AlexNet
 import os
 import time
@@ -32,25 +32,23 @@ eval_resnet34 = models.resnet34(pretrained=False)
 state = torch.load("models/resnet/model.pth")
 eval_resnet34.load_state_dict(state)
 
-#Transfer testing for hybrid, select first architecture to generate perturbations with, select second architecture to test against, epsilon value, and csv name
-def TransferTestingHybrid(net, net2, eps, csvname):
-    net.cuda()
-    net2.cuda()
+#Transfer testing for foolx, select first architecture to generate perturbations with, select second architecture to test against, epsilon value, and csv name
+def TransferTestingFoolX(net, net2, eps, csvname):
     net.eval()
     net2.eval()
-    hybridApproach_Testing_Results = ""
+    foolxApproach_Testing_Results = ""
     Net1Accuracy = 0
-    Net1Hybrid2Accuracy = 0
+    Net1FoolXAccuracy = 0
     Net2Accuracy = 0
-    Net2Hybrid2Accuracy = 0
-    hybridcsv = csvname
+    Net2FoolXAccuracy = 0
+    foolxcsv = csvname
     fieldnames = ['Image', 'Correct Label', 'Network 1 Orig Label', 'Network 2 Orig Label', 'Network 1 Pert Label',
               'Network 2 Pert Label']
 
-    hybrows = []
+    foolxrows = []
     counter = 0
 
-    with open(hybridcsv, 'w', newline='') as csvfile:
+    with open(foolxcsv, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
 
         csvwriter.writerow(fieldnames)
@@ -62,7 +60,7 @@ def TransferTestingHybrid(net, net2, eps, csvname):
     for filename in glob.glob('D:/Imagenet/ILSVRC2012/ILSVRC/Data/CLS-LOC/val/*.JPEG'):  # assuming jpg
         if counter == 5000:
             break
-        print(" \n\n\n**************** Hybrid Approach DeepFool 2 *********************\n")
+        print(" \n\n\n**************** Fool-X *********************\n")
         im_orig = Image.open(filename).convert('RGB')
         print(filename)
         im = transforms.Compose([
@@ -71,7 +69,7 @@ def TransferTestingHybrid(net, net2, eps, csvname):
             transforms.ToTensor(),
             transforms.Normalize(mean=mean,
                                  std=std)])(im_orig)
-        r, loop_i, label_orig, label_pert, pert_image, newf_k = deepfool_hybrid2(im, net)
+        r, loop_i, label_orig, label_pert, pert_image, newf_k = foolx(im, net)
         correct = ILSVRClabels[np.int(counter)].split(' ')[1]
 
         labels = open(os.path.join('synset_words.txt'), 'r').read().split('\n')
@@ -91,9 +89,9 @@ def TransferTestingHybrid(net, net2, eps, csvname):
 
         if (int(label_pert) == int(correct)):
             print("Network 1 Classifier is correct on perturbed image")
-            Net1Hybrid2Accuracy = Net1Hybrid2Accuracy + 1
+            Net1FoolXAccuracy = Net1FoolXAccuracy + 1
 
-        label2 = net2(im[None, :].cuda())
+        label2 = net2(im[None, :].cpu())
         label2 = np.argmax(label2.detach().cpu().numpy())
         str_label_orig2 = labels[np.int(label2)].split(',')[0]
         label_pert2 = net2(pert_image)
@@ -109,12 +107,12 @@ def TransferTestingHybrid(net, net2, eps, csvname):
 
         if (int(label_pert2) == int(correct)):
             print("Network 2 Classifier is correct on perturbed image")
-            Net2Hybrid2Accuracy = Net2Hybrid2Accuracy + 1
+            Net2FoolXAccuracy = Net2FoolXAccuracy + 1
 
         hybrows = []
         hybrows.append(
             [filename[47:75], str_label_correct, str_label_orig, str_label_orig2, str_label_pert, str_label_pert2])
-        with open(hybridcsv, 'a', newline='') as csvfile:
+        with open(foolxcsv, 'a', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
 
             csvwriter.writerows(hybrows)
@@ -122,20 +120,18 @@ def TransferTestingHybrid(net, net2, eps, csvname):
         print("Iterations: " + str(loop_i))
         counter = counter + 1
 
-    with open(hybridcsv, 'a', newline='') as csvfile:
+    with open(foolxcsv, 'a', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerows(["Epsilon: " + str(eps)])
         csvwriter.writerows(["Original Accuracy: " + str(Net1Accuracy / 5000)])
-        csvwriter.writerows(["Original Perturbed Accuracy: " + str(Net1Hybrid2Accuracy / 5000)])
+        csvwriter.writerows(["Original Perturbed Accuracy: " + str(Net1FoolXAccuracy / 5000)])
         csvwriter.writerows(["Transfered Accuracy: " + str(Net2Accuracy / 5000)])
-        csvwriter.writerows(["Transferred Perturbed Accuracy: " + str(Net2Hybrid2Accuracy / 5000)])
+        csvwriter.writerows(["Transferred Perturbed Accuracy: " + str(Net2FoolXAccuracy / 5000)])
         csvwriter.writerows(['Original Network: ' + 'resnet34'])
         csvwriter.writerows(['Transfered Network: ' + 'resnet101'])
 
 #Transfer testing for deepfool, select first architecture to generate perturbations with, select second architecture to test against and csv name
 def TransferTestingDeepfool(net, net2, csvname):
-    net.cuda()
-    net2.cuda()
     net.eval()
     net2.eval()
     Net1Accuracy = 0
@@ -190,7 +186,7 @@ def TransferTestingDeepfool(net, net2, csvname):
             print("Network 1 Classifier is correct on perturbed image")
             Net1DeepfoolAccuracy = Net1DeepfoolAccuracy + 1
 
-        label2 = net2(im[None, :].cuda())
+        label2 = net2(im[None, :].cpu())
         label2 = np.argmax(label2.detach().cpu().numpy())
         str_label_orig2 = labels[np.int(label2)].split(',')[0]
         label_pert2 = net2(pert_image)
@@ -230,8 +226,6 @@ def TransferTestingDeepfool(net, net2, csvname):
 
 #Transfer testing for FGSM, select first architecture to generate perturbations with, select second architecture to test against, epsilon value, and csv name
 def TransferTestingFGSM(net, net2, eps, csvname):
-    net.cuda()
-    net2.cuda()
     net.eval()
     net2.eval()
     Net1Accuracy = 0
@@ -264,14 +258,14 @@ def TransferTestingFGSM(net, net2, eps, csvname):
         img = (img - mean) / std
         img = img.transpose(2, 0, 1)
 
-        inp = Variable(torch.from_numpy(img).to('cuda:0').float().unsqueeze(0), requires_grad=True)
+        inp = Variable(torch.from_numpy(img).to('cpu').float().unsqueeze(0), requires_grad=True)
 
         out = net(inp)
         out2 = net2(inp)
         criterion = nn.CrossEntropyLoss()
         pred = np.argmax(out.data.cpu().numpy())
         pred2 = np.argmax(out2.data.cpu().numpy())
-        loss = criterion(out, Variable(torch.Tensor([float(pred)]).to('cuda:0').long()))
+        loss = criterion(out, Variable(torch.Tensor([float(pred)]).to('cpu').long()))
         loss.backward()
         print('Network 1 Prediction before attack: %s' % (classes[pred].split(',')[0]))
         print('Network 2 Prediction before attack: %s' % (classes[pred2].split(',')[0]))
@@ -327,20 +321,18 @@ def TransferTestingFGSM(net, net2, eps, csvname):
         csvwriter.writerows(['Original Network: ' + 'resnet34'])
         csvwriter.writerows(['Transfered Network: ' + 'resnet101'])
 
-#Transfer testing for hybrid with CIFAR10 dataset, select first architecture to generate perturbations with, select second architecture to test against, epsilon value, and csv name
-def CIFARHybridTesting(original_net, transfer_net, eps, csvname):
-    original_net.cuda()
-    transfer_net.cuda()
+#Transfer testing for foolx with CIFAR10 dataset, select first architecture to generate perturbations with, select second architecture to test against, epsilon value, and csv name
+def CIFARFoolXTesting(original_net, transfer_net, eps, csvname):
     original_net.eval()
     transfer_net.eval()
     Net1Accuracy = 0
-    Net1Hybrid2Accuracy = 0
+    Net1FoolXAccuracy = 0
     Net2Accuracy = 0
-    Net2Hybrid2Accuracy = 0
-    hybridcsv = csvname
+    Net2FoolXAccuracy = 0
+    foolxcsv = csvname
     fieldnames = ['Image', 'Correct Label', 'Network 1 Orig Label', 'Network 2 Orig Label', 'Network 1 Pert Label',
                   'Network 2 Pert Label']
-    with open(hybridcsv, 'w', newline='') as csvfile:
+    with open(foolxcsv, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(fieldnames)
     classes = ('plane', 'car', 'bird', 'cat',
@@ -354,13 +346,13 @@ def CIFARHybridTesting(original_net, transfer_net, eps, csvname):
                                            download=False, transform=transform)
 
     for i,data in enumerate(testset):
-        print("\n\n\n\n\n\n\n\n\n****************Hybrid Testing *********************\n")
+        print("\n\n\n\n\n\n\n\n\n**************** Fool-X Testing *********************\n")
         inputs, labels = data
         filename = i
         if i == 5000:
             break
         start_time = time.time()
-        r, loop_i, label_orig, label_pert, pert_image, newf_k = deepfool_hybrid2(inputs, original_net, eps)
+        r, loop_i, label_orig, label_pert, pert_image, p, newf_k = foolx(inputs, original_net, eps)
         correct = labels
         str_label_correct = classes[np.int(correct)]
         str_label_orig = classes[np.int(label_orig)]
@@ -375,8 +367,8 @@ def CIFARHybridTesting(original_net, transfer_net, eps, csvname):
 
         if (int(label_pert) == int(correct)):
             print("Network 1 Classifier is correct on perturbed image")
-            Net1Hybrid2Accuracy = Net1Hybrid2Accuracy + 1
-        label2 = transfer_net(inputs[None, ...].cuda())
+            Net1FoolXAccuracy = Net1FoolXAccuracy + 1
+        label2 = transfer_net(inputs[None, ...].cpu())
         label2 = np.argmax(label2.detach().cpu().numpy())
         str_label_orig2 = classes[np.int(label2)].split(',')[0]
         label_pert2 = transfer_net(pert_image)
@@ -392,20 +384,20 @@ def CIFARHybridTesting(original_net, transfer_net, eps, csvname):
 
         if (int(label_pert2) == int(correct)):
             print("Network 2 Classifier is correct on perturbed image")
-            Net2Hybrid2Accuracy = Net2Hybrid2Accuracy + 1
+            Net2FoolXAccuracy = Net2FoolXAccuracy + 1
         hybrows = []
         hybrows.append(
             [filename, str_label_correct, str_label_orig, str_label_orig2, str_label_pert, str_label_pert2])
-        with open(hybridcsv, 'a', newline='') as csvfile:
+        with open(foolxcsv, 'a', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerows(hybrows)
-    with open(hybridcsv, 'a', newline='') as csvfile:
+    with open(foolxcsv, 'a', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerows(["Epsilon: " + str(eps)])
         csvwriter.writerows(["Original Accuracy: " + str(Net1Accuracy / 5000)])
-        csvwriter.writerows(["Original Perturbed Accuracy: " + str(Net1Hybrid2Accuracy / 5000)])
+        csvwriter.writerows(["Original Perturbed Accuracy: " + str(Net1FoolXAccuracy / 5000)])
         csvwriter.writerows(["Transfered Accuracy: " + str(Net2Accuracy / 5000)])
-        csvwriter.writerows(["Transfered Perturbed Accuracy: " + str(Net2Hybrid2Accuracy / 5000)])
+        csvwriter.writerows(["Transfered Perturbed Accuracy: " + str(Net2FoolXAccuracy / 5000)])
         csvwriter.writerows(['Original Network: ' + 'resnet34'])
         csvwriter.writerows(['Transfered Network: ' + 'resnet101'])
 
@@ -432,24 +424,48 @@ def CIFARHybridTesting(original_net, transfer_net, eps, csvname):
 #TransferTestingFGSM(orig_alexnet, eval_resnet34, 0.05, 'transfer_fgsm_alexnet_to_resnet34_0.05.csv')
 #TransferTestingFGSM(orig_alexnet, eval_resnet34, 0.0005, 'transfer_fgsm_alexnet_to_resnet34_0.0005.csv')
 
-#TransferTestingHybrid(orig_resnet34, eval_resnet101, 0.2, 'transfer_hybrid_resnet34_to_resnet101_0.2.csv')
-#TransferTestingHybrid(orig_resnet34, eval_googlenet, 0.2, 'transfer_hybrid_resnet34_to_googlenet_0.2.csv')
-#TransferTestingHybrid(orig_resnet34, eval_alexnet, 0.2, 'transfer_hybrid_resnet34_to_alexnet_0.2.csv')
-#TransferTestingHybrid(orig_resnet101, eval_resnet34, 0.2, 'transfer_hybrid_resnet101_to_resnet34_0.2.csv')
-#TransferTestingHybrid(orig_resnet101, eval_googlenet, 0.2, 'transfer_hybrid_resnet101_to_googlenet_0.2.csv')
-#TransferTestingHybrid(orig_alexnet, eval_googlenet, 0.2, 'transfer_hybrid_alexnet_to_googlenet_0.2.csv')
-#TransferTestingHybrid(orig_alexnet, eval_resnet34, 0.2, 'transfer_hybrid_alexnet_to_resnet34_0.2.csv')
-#TransferTestingHybrid(orig_googlenet, eval_resnet101, 0.2, 'transfer_hybrid_googlenet_to_resnet101_0.2.csv')
-#TransferTestingHybrid(orig_googlenet, eval_resnet34, 0.2, 'transfer_hybrid_googlenet_to_resnet34_0.2.csv')
-#TransferTestingHybrid(orig_googlenet, eval_alexnet, 0.2, 'transfer_hybrid_googlenet_to_alexnet_0.2.csv')
+#TransferTestingFoolX(orig_resnet34, eval_resnet101, 0.2, 'transfer_foolx_resnet34_to_resnet101_0.2.csv')
+#TransferTestingFoolX(orig_resnet34, eval_googlenet, 0.2, 'transfer_foolx_resnet34_to_googlenet_0.2.csv')
+#TransferTestingFoolX(orig_resnet34, eval_alexnet, 0.2, 'transfer_foolx_resnet34_to_alexnet_0.2.csv')
+#TransferTestingFoolX(orig_resnet101, eval_resnet34, 0.2, 'transfer_foolx_resnet101_to_resnet34_0.2.csv')
+#TransferTestingFoolX(orig_resnet101, eval_googlenet, 0.2, 'transfer_foolx_resnet101_to_googlenet_0.2.csv')
+#TransferTestingFoolX(orig_alexnet, eval_googlenet, 0.2, 'transfer_foolx_alexnet_to_googlenet_0.2.csv')
+#TransferTestingFoolX(orig_alexnet, eval_resnet34, 0.2, 'transfer_foolx_alexnet_to_resnet34_0.2.csv')
+#TransferTestingFoolX(orig_googlenet, eval_resnet101, 0.2, 'transfer_foolx_googlenet_to_resnet101_0.2.csv')
+#TransferTestingFoolX(orig_googlenet, eval_resnet34, 0.2, 'transfer_foolx_googlenet_to_resnet34_0.2.csv')
+#TransferTestingFoolX(orig_googlenet, eval_alexnet, 0.2, 'transfer_foolx_googlenet_to_alexnet_0.2.csv')
 
 #TransferTestingFGSM(orig_resnet101, source, 0.01, 'transfer_fgsm_googlenet_to_resnet101_0.01.csv')
 #TransferTestingDeepfool(orig_resnet101, source, 'transfer_deepfool_googlenet_to_resnet101.csv')
-#TransferTestingHybrid(net, net2, 0.01)
+#TransferTestingFoolX(net, net2, 0.01)
 #TransferTestingFGSM(orig_resnet101, source, 0.2, 'transfer_fgsm_googlenet_to_resnet101_0.2.csv')
 #TransferTestingFGSM(orig_resnet101, source, 0.05, 'transfer_fgsm_googlenet_to_resnet101_0.05.csv')
 #TransferTestingFGSM(orig_resnet101, source, 0.0005, 'transfer_fgsm_googlenet_to_resnet101_0.0005.csv')
 
-CIFARHybridTesting(orig_alexnet, eval_resnet34, 0.0005, 'transfer_hybrid_CIFAR10_alexnet_to_resnet34_0.0005')
-CIFARHybridTesting(orig_alexnet, eval_resnet34, 0.05, 'transfer_hybrid_CIFAR10_alexnet_to_resnet34_0.05')
-CIFARHybridTesting(orig_alexnet, eval_resnet34, 0.2, 'transfer_hybrid_CIFAR10_alexnet_to_resnet34_0.2')
+# CIFARFoolXTesting(orig_alexnet, eval_resnet34, 0.0005, 'transfer_foolx_CIFAR10_alexnet_to_resnet34_0.0005.csv')
+# CIFARFoolXTesting(orig_alexnet, eval_resnet34, 0.05, 'transfer_foolx_CIFAR10_alexnet_to_resnet34_0.05.csv')
+# CIFARFoolXTesting(orig_alexnet, eval_resnet34, 0.2, 'transfer_foolx_CIFAR10_alexnet_to_resnet34_0.2.csv')
+#
+# CIFARFoolXTesting(orig_alexnet, eval_googlenet, 0.0005, 'transfer_foolx_CIFAR10_alexnet_to_googlenet_0.0005.csv')
+# CIFARFoolXTesting(orig_alexnet, eval_googlenet, 0.05, 'transfer_foolx_CIFAR10_alexnet_to_googlenet_0.05.csv')
+# CIFARFoolXTesting(orig_alexnet, eval_googlenet, 0.2, 'transfer_foolx_CIFAR10_alexnet_to_googlenet_0.2.csv')
+#
+# CIFARFoolXTesting(orig_googlenet, eval_alexnet, 0.0005, 'transfer_foolx_CIFAR10_googlenet_to_alexnet_0.0005.csv')
+# CIFARFoolXTesting(orig_googlenet, eval_alexnet, 0.05, 'transfer_foolx_CIFAR10_googlenet_to_alexnet_0.05.csv')
+# CIFARFoolXTesting(orig_googlenet, eval_alexnet, 0.2, 'transfer_foolx_CIFAR10_googlenet_to_alexnet_0.2.csv')
+#
+# CIFARFoolXTesting(orig_googlenet, eval_resnet34, 0.0005, 'transfer_foolx_CIFAR10_googlenet_to_resnet34_0.0005.csv')
+# CIFARFoolXTesting(orig_googlenet, eval_resnet34, 0.05, 'transfer_foolx_CIFAR10_googlenet_to_resnet34_0.05.csv')
+# CIFARFoolXTesting(orig_googlenet, eval_resnet34, 0.2, 'transfer_foolx_CIFAR10_googlenet_to_resnet34_0.2.csv')
+#
+# CIFARFoolXTesting(orig_resnet34, eval_alexnet, 0.0005, 'transfer_foolx_CIFAR10_resnet34_to_alexnet_0.0005.csv')
+# CIFARFoolXTesting(orig_resnet34, eval_alexnet, 0.05, 'transfer_foolx_CIFAR10_resnet34_to_alexnet_0.05.csv')
+# CIFARFoolXTesting(orig_resnet34, eval_alexnet, 0.2, 'transfer_foolx_CIFAR10_resnet34_to_alexnet_0.2.csv')
+#
+# CIFARFoolXTesting(orig_resnet34, eval_googlenet, 0.0005, 'transfer_foolx_CIFAR10_resnet34_to_googlenet_0.0005.csv')
+# CIFARFoolXTesting(orig_resnet34, eval_googlenet, 0.05, 'transfer_foolx_CIFAR10_resnet34_to_googlenet_0.05.csv')
+# CIFARFoolXTesting(orig_resnet34, eval_googlenet, 0.2, 'transfer_foolx_CIFAR10_resnet34_to_googlenet_0.2.csv')
+
+CIFARFoolXTesting(orig_alexnet, eval_resnet34, 0.0005, 'transfer_foolx_CIFAR10_alexnet_to_resnet34_0.0005')
+CIFARFoolXTesting(orig_alexnet, eval_resnet34, 0.05, 'transfer_foolx_CIFAR10_alexnet_to_resnet34_0.05')
+CIFARFoolXTesting(orig_alexnet, eval_resnet34, 0.2, 'transfer_foolx_CIFAR10_alexnet_to_resnet34_0.2')

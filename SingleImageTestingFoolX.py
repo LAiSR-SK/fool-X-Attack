5e43f2fc5e1becde6fe60a84fa519f97addaa4dd
The Network & Software Security Research (NSSR) lab <86532171+nssrlab@users.noreply.github.com>
2021-12-28 21:53:07 -0500

    Update SingleImageTestingFoolX.py

--- SingleImageTestingFoolX.py
+++ SingleImageTestingFoolX.py
@@ -9,12 +9,24 @@ from foolx import foolx
 import os
 import time
 
+#Check if cuda is available.
+is_cuda = torch.cuda.is_available()
 torch.device('cpu')
+
+#If cuda is available use GPU for faster processing, if not, use CPU.
+if is_cuda:
+    print("Using GPU")
+    torch.device('cuda')
+else:
+    print("Using CPU")
+
 
 net = models.resnet34(pretrained=True)
 #net = models.alexnet(pretrained=True)
 # Switch to evaluation mode
 net.eval()
+if is_cuda:
+    net.cuda()
 
 im_orig = Image.open('new/ILSVRC2017_test_00004324.JPEG').convert('RGB')
 
@@ -57,36 +69,29 @@ tf = transforms.Compose([transforms.Normalize(mean=[0, 0, 0], std=list(map(lambd
                         transforms.ToPILImage(),
                         transforms.CenterCrop(224)])
 
-imagetransform = transforms.Compose([transforms.Normalize(mean=[0, 0, 0], std=list(map(lambda x: 1 / x, std))),
-                        transforms.Normalize(list(map(lambda x: -x, mean)), std=[1, 1, 1]),
-                        transforms.Lambda(clip)])
-
-tensortransform = transforms.Compose([transforms.Scale(256),
-                        transforms.CenterCrop(224),
-                        transforms.ToTensor(),
-                        transforms.Normalize(mean=[0, 0, 0], std=list(map(lambda x: 1 / x, std))),
-                        transforms.Normalize(list(map(lambda x: -x, mean)), std=[1, 1, 1]),
-                        transforms.Lambda(clip)])
 plt.figure()
 plt.imshow(tf(im.cpu()))
 plt.title(str_label_orig)
+tf(im.cpu()).save('orig.png')
 plt.show()
-
 plt.figure()
 plt.imshow(tf(pert_image.cpu()[0]))
+img = tf(pert_image.cpu()[0])
+img.save('image.png')
 plt.title(str_label_pert)
 plt.show()
 print(loop_i)
-
 plt.figure()
-plt.imshow(tf(pert.cpu()[0]))
+fc = 1000000
+plt.imshow(tf(pert.cpu()[0]*fc))
+tf(pert.cpu()[0]*fc).save('perturbation.png')
+plt.title(str_label_pert)
 plt.show()
-print(imagetransform(pert_image.cpu()[0]))
-print("im orig")
-print(tensortransform(im_orig))
-print("difference:")
-diff = imagetransform(pert_image.cpu()[0]) - tensortransform(im_orig)
-print(diff)
-fro = np.linalg.norm(diff.numpy())
-print(fro)
-print(torch.mean(torch.abs(diff)))
+
+#10000000(0) - 0.0005
+#1000000(0) - 0.005
+#10000 - 0.05
+#100 - 0.1
+#100000 - 0.01
+#10000000 - 0.001
+#10000000000 - 0.0001
